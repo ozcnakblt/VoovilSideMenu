1f0d3b933bb679a8f17dcca63d29214a79410b03
Özcan Akbulut <ozcanakbulut@Ozcans-MacBook-Pro.local>
2013-10-24 03:16:23 +0300

    Initial commit

---
+++ VoovilSideMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VoovilSideMenu
+//
+//  Created by Özcan Akbulut on 23.10.13.
+//  Copyright (c) 2013 Özcan Akbulut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ VoovilSideMenuTests/VoovilSideMenuTests.m
@@ -0,0 +1,34 @@
+//
+//  VoovilSideMenuTests.m
+//  VoovilSideMenuTests
+//
+//  Created by Özcan Akbulut on 23.10.13.
+//  Copyright (c) 2013 Özcan Akbulut. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VoovilSideMenuTests : XCTestCase
+
+@end
+
+@implementation VoovilSideMenuTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
