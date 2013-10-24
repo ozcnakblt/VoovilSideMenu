aaff7e471e20133a2fde2e81bbc54c31307efe64
Özcan Akbulut <ozcanakbulut@Ozcans-MacBook-Pro.local>
2013-10-24 03:14:38 +0300

    Initial commit

---
+++ VoovilSideMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VoovilSideMenu
+//
+//  Created by Özcan Akbulut on 23.10.13.
+//  Copyright (c) 2013 Özcan Akbulut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
